3bddd0af6a00bf20a08c52864df1ad8deda887f0
NGShpagin <shpagin9393@mail.ru>
2023-07-27 13:13:04 +0300

    add logic (script.js) for action (add to cart) for main page (index.html)

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ async function fetchData() {
         }
         const data = await response.json()
         const productBox = document.querySelector('.products-box')
-        data.forEach(({image, name, description, price}) => {
+        data.forEach(({image, name, description, price, color, quantity, size}) => {
             const productEl = `
-            <a class="product" href="product.html">
+            <div class="product">
                 <div class="product__image">
-                    <img src="${image}" alt="${name}">
+                    <img src="${image}" alt="${image}">
                     <div class="hover-content">
                         <svg width="27" height="25" viewBox="0 0 27 25" fill="none"
                              xmlns="http://www.w3.org/2000/svg">
@@ -25,7 +25,7 @@ async function fetchData() {
                     <p class="product__text">${description}</p>
                     <p class="product__price">$${price}</p>
                 </div>
-            </a>`
+            </div>`
             productBox.insertAdjacentHTML('beforeend', productEl);
         })
     } catch (error) {
@@ -33,4 +33,78 @@ async function fetchData() {
     }
 }
 
-fetchData();
+
+
+async function addCart(element) {
+    const productEl = element.closest('.product');
+    const productName = productEl.querySelector('.product__name');
+    try {
+        const response = await fetch('featured_items.json');
+        if (!response.ok) {
+            throw new Error('Не удалось обработать data.json')
+        }
+        const data = await response.json()
+        const cartSection = document.querySelector('.cart-section')
+        const cartBox = cartSection.querySelector('.cart-box')
+        data.forEach(({image, name, description, price, color, size}) => {
+            if (productName.textContent === name) {
+                const itemCart = `
+                    <article class="cart-item">
+                        <a href="#">
+                            <img src="${image}" alt="${image}">
+                        </a>
+                        <div class="cart-item__description">
+                            <div class="cart-item__box">
+                                <a href="product.html">${name}</a>
+                                <div class="cart-item__box-description">
+                                    <p>Price: <b class="cart-item__price">$${price}</b></p>
+                                    <p>Color: ${color}</p>
+                                    <p>Size: ${size} </p>
+                                    <label class="cart-item__quantity">Quantity:
+                                        <input type="number" min="1" value="1"></label>
+                                </div>
+                             </div>
+                            <svg class="cart-item__close" width="18" height="18" viewBox="0 0 18 18" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M11.2453 9L17.5302 2.71516C17.8285 2.41741 17.9962 2.01336 17.9966 1.59191C17.997 1.17045 17.8299 0.76611 17.5322 0.467833C17.2344 0.169555 16.8304 0.00177586 16.4089 0.00140366C15.9875 0.00103146 15.5831 0.168097 15.2848 0.465848L9 6.75069L2.71516 0.465848C2.41688 0.167571 2.01233 0 1.5905 0C1.16868 0 0.764125 0.167571 0.465848 0.465848C0.167571 0.764125 0 1.16868 0 1.5905C0 2.01233 0.167571 2.41688 0.465848 2.71516L6.75069 9L0.465848 15.2848C0.167571 15.5831 0 15.9877 0 16.4095C0 16.8313 0.167571 17.2359 0.465848 17.5342C0.764125 17.8324 1.16868 18 1.5905 18C2.01233 18 2.41688 17.8324 2.71516 17.5342L9 11.2493L15.2848 17.5342C15.5831 17.8324 15.9877 18 16.4095 18C16.8313 18 17.2359 17.8324 17.5342 17.5342C17.8324 17.2359 18 16.8313 18 16.4095C18 15.9877 17.8324 15.5831 17.5342 15.2848L11.2453 9Z"
+                            fill="#575757"/>
+                    </svg>
+                        </div>
+                    </article>`
+                cartBox.insertAdjacentHTML('beforeend', itemCart);
+            }
+        })
+        const cartItemsArray = cartBox.querySelectorAll('.cart-item')
+        if (cartItemsArray.length > 0) cartSection.style.display = "block";
+        const deleteBtn = document.querySelectorAll('.cart-item__close');
+        deleteBtn.forEach((button) => {
+            button.addEventListener('click', () => {
+                const productEl = button.closest('.cart-item');
+                productEl.remove();
+                const cartSection = document.querySelector('.cart-section')
+                const cartBox = cartSection.querySelector('.cart-box')
+                const cartItemsArray = cartBox.querySelectorAll('.cart-item')
+                if (cartItemsArray.length === 0) cartSection.style.display = "none";
+            })
+        })
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+fetchData().then(r => {
+    const hoverContentEl = document.querySelectorAll('.product');
+    hoverContentEl.forEach((element) => {
+        element.addEventListener('click', () => {
+            console.log(element)
+            addCart(element)
+        });
+    })
+})
+
+
+
+
+
+
